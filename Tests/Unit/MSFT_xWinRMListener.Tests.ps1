0911fcfc16c7812108ab63454cccc7e86781b00e
tragiccode <michael@tragiccode.com>
2017-09-09 12:42:10 -0500

    Added test to make sure dsc resource is well formed

--- Tests/Unit/MSFT_xWinRMListener.Tests.ps1
+++ Tests/Unit/MSFT_xWinRMListener.Tests.ps1
@@ -5,6 +5,10 @@ Describe 'xWinRMListener' {
         Test-xDscSchema "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener\MSFT_xWinRMListener.schema.mof"
     }
 
+    It 'is a well formed resource' {
+        Test-xDscResource "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener"
+    }
+
     Describe 'Get-TargetResource' {
     }
 
