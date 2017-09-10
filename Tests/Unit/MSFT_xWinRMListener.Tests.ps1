28b9c9c1f1aaad2b8186d38d0f0329b7d18b6301
tragiccode <michael@tragiccode.com>
2017-09-09 22:54:21 -0500

    Get-TargetResource should return a hash

--- Tests/Unit/MSFT_xWinRMListener.Tests.ps1
+++ Tests/Unit/MSFT_xWinRMListener.Tests.ps1
@@ -1,22 +1,40 @@
+$Global:DSCResourceName = 'MSFT_xWinRMListener'  #<----- Just change this
 
-Describe 'xWinRMListener' {
+Import-Module "$($PSScriptRoot)\..\..\DSCResources\$($Global:DSCResourceName)\$($Global:DSCResourceName).psm1" -Force
 
-    It 'is syntactically correct' {
-        Test-xDscSchema "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener\MSFT_xWinRMListener.schema.mof"
+InModuleScope $Global:DSCResourceName {
+    Describe 'xWinRMListener' {
+
+        $mockParameters = @{
+            Address = '127.0.0.1'
+            Transport = 'HTTP'
+        }
+
+        It 'is syntactically correct' {
+            Test-xDscSchema "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener\MSFT_xWinRMListener.schema.mof"
+        }
+
+        It 'is a well formed resource' {
+            Test-xDscResource "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener"
+        }
+
+        Describe 'Get-TargetResource' {
+
+            It 'returns a hashtable' {
+                (Get-TargetResource @mockParameters) | Should BeOfType [Hashtable]
+            }
+        }
     }
-
-    It 'is a well formed resource' {
-        Test-xDscResource "$PSScriptRoot\..\..\DSCResources\MSFT_xWinRMListener"
-    }
-
-    Describe 'Get-TargetResource' {
-    }
-
 
     Describe 'Set-TargetResource' {
     }
 
     Describe 'Test-TargetResource' {
+    }
+
+
+    Describe 'Get-WinRMListeners' {
+
     }
 
 }
