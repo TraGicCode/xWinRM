2f7180117baca8a4cd5b54cd5872f8c1e63f94db
tragiccode <michael@tragiccode.com>
2017-09-11 22:17:22 -0500

    PSScriptAnalyzer

--- Tests/Unit/StyleConventions.Tests.ps1
+++ Tests/Unit/StyleConventions.Tests.ps1
@@ -1,66 +1,15 @@
 
 Describe 'StyleConventions' {
     Import-Module -Name PSScriptAnalyzer
-    
-            $requiredPssaRuleNames = @(
-                'PSAvoidDefaultValueForMandatoryParameter',
-                'PSAvoidDefaultValueSwitchParameter',
-                'PSAvoidInvokingEmptyMembers',
-                'PSAvoidNullOrEmptyHelpMessageAttribute',
-                'PSAvoidUsingCmdletAliases',
-                'PSAvoidUsingComputerNameHardcoded',
-                'PSAvoidUsingDeprecatedManifestFields',
-                'PSAvoidUsingEmptyCatchBlock',
-                'PSAvoidUsingInvokeExpression',
-                'PSAvoidUsingPositionalParameters',
-                'PSAvoidShouldContinueWithoutForce',
-                'PSAvoidUsingWMICmdlet',
-                'PSAvoidUsingWriteHost',
-                'PSDSCReturnCorrectTypesForDSCFunctions',
-                'PSDSCStandardDSCFunctionsInResource',
-                'PSDSCUseIdenticalMandatoryParametersForDSC',
-                'PSDSCUseIdenticalParametersForDSC',
-                'PSMissingModuleManifestField',
-                'PSPossibleIncorrectComparisonWithNull',
-                'PSProvideCommentHelp',
-                'PSReservedCmdletChar',
-                'PSReservedParams',
-                'PSUseApprovedVerbs',
-                'PSUseCmdletCorrectly',
-                'PSUseOutputTypeCorrectly'
-            )
-    
-            $flaggedPssaRuleNames = @(
-                'PSAvoidGlobalVars',
-                'PSAvoidUsingConvertToSecureStringWithPlainText',
-                'PSAvoidUsingPlainTextForPassword',
-                'PSAvoidUsingUsernameAndPasswordParams',
-                'PSDSCUseVerboseMessageInDSCResource',
-                'PSShouldProcess',
-                'PSUseDeclaredVarsMoreThanAssigments',
-                'PSUsePSCredentialType'
-            )
-    
-            $ignorePssaRuleNames = @(
-                'PSDSCDscExamplesPresent',
-                'PSDSCDscTestsPresent',
-                'PSUseBOMForUnicodeEncodedFile',
-                'PSUseShouldProcessForStateChangingFunctions',
-                'PSUseSingularNouns',
-                'PSUseToExportFieldsInManifest',
-                'PSUseUTF8EncodingForHelpFile'
-            )
     $dscResourceModules = Get-ChildItem -Path "$PSScriptRoot\..\..\DSCResources\**\*.psm1"
-    $dscResourceModules
-    Write-Host $result
-    $result = Invoke-ScriptAnalyzer -Path $dscResourceModules -IncludeRule ($requiredPssaRuleNames + $flaggedPssaRuleNames + $ignorePssaRuleNames) -Severity Error
+    $result = Invoke-ScriptAnalyzer -Path $dscResourceModules -Severity Error, Warning
     
-    It 'Has no style rules broken' {
+    It 'has no error or warning severity rules broken' {
 
         Write-Warning -Message 'The following PSScriptAnalyzer errors need to be fixed:'
         foreach ($errorRecord in $result)
         {
-            Write-Warning -Message "$($errorRecord.ScriptName) (Line $($errorRecord.Line)): $($errorRecord.Message)"
+            Write-Warning -Message "[$($errorRecord.Severity)] - $($errorRecord.ScriptName) (Line $($errorRecord.Line)): $($errorRecord.Message)"
         }
 
         $result | Should Be $null
