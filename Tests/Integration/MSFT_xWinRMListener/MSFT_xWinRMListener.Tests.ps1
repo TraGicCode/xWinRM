3a31f866e35c0e38a2f2a83bdebb62542549d707
tragiccode <michael@tragiccode.com>
2017-09-12 23:02:17 -0500

    go

--- Tests/Integration/MSFT_xWinRMListener/MSFT_xWinRMListener.Tests.ps1
+++ Tests/Integration/MSFT_xWinRMListener/MSFT_xWinRMListener.Tests.ps1
@@ -1,9 +1,13 @@
+function Flatten($target)
+{
+  return [String]::Join("",$target)
+}
 
 describe 'WinRM HTTP Listener' {
-  $result = winrm get winrm/config/Listener?Address=*+Transport=http
-
+  $normalResult = winrm get winrm/config/Listener?Address=*+Transport=http
+  $result = Flatten($normalResult)
   It "has the expected properties" {
-    $result | Should BeLike "Address = `*"
-    $result | Should BeLike "Transport = http"
+    $result | Should BeLike "*Address = `**"
+    $result | Should BeLike "*Transport = http*"
   }
 }
